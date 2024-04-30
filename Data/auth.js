57a2abe0e2669b96c5ecd3b9ac88bf90cac08b43
gane <gwa_ne@naver.com>
2024-04-30 10:27:36 +0900

    회원가입, 로그인 라우터 추가

---
+++ Data/auth.js
@@ -0,0 +1,37 @@
+
+let users = [
+    {
+        id: '1',
+        username: 'apple',
+        password: '1111',
+        name: '김사과',
+        email: 'apple@apple.com',
+        url: 'https://cdn.pointe.co.kr/news/photo/202110/4299_11148_437.jpg'
+        },
+    {
+        id: '2',
+        username: 'banana',
+        password: '2222',
+        name: '반하나',
+        email: 'banana@banana.com',
+        url: 'https://cdn.pointe.co.kr/news/photo/202110/4299_11148_437.jpg'
+    }
+];
+
+export async function createUser(username, password, name, email) {
+    const user = {
+        id: '10',
+        username,
+        password,
+        name,
+        email,
+        url: 'https://cdn.pointe.co.kr/news/photo/202110/4299_11148_437.jpg'
+    }
+    users = [user, ...users]
+    return users;
+}
+
+export async function login(username) {
+    const user = users.find((user) => user.username ===username)
+    return user;
+}
--- controller/tweet.js
+++ controller/tweet.js
@@ -13,7 +13,7 @@ export async function getTweet(req, res, next) {
     const id = req.params.id;
     const tweet = await tweetRepository.getbyId(id);
     if(tweet){
-        res.statue(200).json(tweet);
+        res.status(200).json(tweet);
     }else{
         res.status(404).json({message: `${id}의 트읫이 없습니다`})
     }
@@ -22,6 +22,7 @@ export async function getTweet(req, res, next) {
 // 트윗을 생성하는 함수
 export async function createTweet(req, res, next) {
     const {text, name, username} = req.body;
+    body('text').trim().isLength({min:3}).withMessage('텍스트는 세글자 이상으로 입력!')
     const tweet = await tweetRepository.create(text, name, username);
     res.status(201).json(tweet);
 };
