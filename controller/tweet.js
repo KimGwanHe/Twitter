c9027bb5a7d700afc49a87cc2b3d6291e1cb7817
gane <gwa_ne@naver.com>
2024-05-17 11:45:08 +0900

    소켓적용

--- controller/tweet.js
+++ controller/tweet.js
@@ -1,4 +1,5 @@
 import * as tweetRepository from '../Data/tweet.js';
+import { getSocketIo } from '../connection/socket.js';
 
 // 여러 트윗을 가져오는 함수
 export async function getTweets(req, res) {
@@ -25,6 +26,7 @@ export async function createTweet(req, res, next) {
     // body('text').trim().isLength({min:3}).withMessage('텍스트는 세글자 이상으로 입력!')
     const tweet = await tweetRepository.create(text, req.userId);
     res.status(201).json(tweet);
+    getSocketIo().emit('tweets', tweet)
 };
 
 // 트윗을 변경하는 함수
